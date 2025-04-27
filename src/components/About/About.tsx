9a307a3b4e85df1f45a8616df7f1bc92cf5efb3e
Mattia-FR <mattia.bouvier@protonmail.com>
2025-04-27 19:15:41 +0200

    first commit

---
+++ src/components/About/About.tsx
@@ -0,0 +1,12 @@
+import "./About.css";
+
+function About() {
+  return (
+    <section>
+      <h1>Titre H1</h1>
+      <p>Paragraphe</p>
+    </section>
+  );
+}
+
+export default About;
---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,12 @@
+import "./Hero.css";
+
+function Hero() {
+  return (
+    <section>
+      <h1>Titre H1</h1>
+      <p>Paragraphe</p>
+    </section>
+  );
+}
+
+export default Hero;
