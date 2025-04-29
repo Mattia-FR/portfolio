c516eb6bd08974b2925118df0adaab3e451d7f28
Mattia-FR <mattia.bouvier@protonmail.com>
2025-04-30 01:00:29 +0200

    Progressing on the Contact section.

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -4,11 +4,21 @@ function Contact() {
   return (
     <main className="mainContact" id="contact">
       <h2>&lt;contact /&gt;</h2>
-      <ul>
-        <li>mail : mattia.bouvier@protonmail.com</li>
-        <li>linkedin : https://www.linkedin.com/in/mattia-bouvier/</li>
-        <li>github : https://github.com/Mattia-FR</li>
-      </ul>
+      <section className="iconsContainer">
+        <a href="mailto:mattia.bouvier@protonmail.com">
+          <img
+            src="/images/protonmail.svg"
+            alt="Protonmail"
+            className="icons"
+          />
+        </a>
+        <a href="https://www.linkedin.com/in/mattia-bouvier">
+          <img src="/images/linkedin.svg" alt="LinkedIn" className="icons" />
+        </a>
+        <a href="https://github.com/Mattia-FR">
+          <img src="/images/github.svg" alt="GitHub" className="icons" />
+        </a>
+      </section>
       <section className="navContact">
         <a href="#hero">
           <button type="button">[ accueil ]</button>
