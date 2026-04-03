422d6fcb77965d4836b16e7e726f4c869e482f5e
Mattia-FR <mattia.bouvier@protonmail.com>
2026-04-03 13:52:56 +0200

    Added link in the footer, style in modal and contact page.

--- src/components/organisms/Footer.tsx
+++ src/components/organisms/Footer.tsx
@@ -4,6 +4,9 @@ function Footer() {
   return (
     <footer className="footer">
       <p className="footer__text">Codé avec les pieds, amour et React.</p>
+      <a href="https://github.com/Mattia-FR/portfolio" className="footer__link">
+        Code source
+      </a>
     </footer>
   );
 }
--- src/components/pages/ContactPage.tsx
+++ src/components/pages/ContactPage.tsx
@@ -1,27 +1,34 @@
+import github from "../../assets/github-icon.png";
+import linkedin from "../../assets/linkedin-icon.png";
 import "./ContactPage.css";
 
 function ContactPage() {
   return (
     <section className="contact-page">
-      <h1 className="contact-page__title">Contactez-moi</h1>
+      <h1 className="contact-page__title">Me contacter</h1>
       <p className="contact-page__description">
-        Disponible pour un poste de développeur web junior. N'hésitez pas à me
-        contacter sur LinkedIn ou à consulter mon GitHub.
+        <span className="pink">{"<"}</span> Disponible pour un poste de
+        développeur web. N'hésitez pas à me contacter sur LinkedIn ou à
+        consulter mon GitHub. <span className="pink">{"/>"}</span>
       </p>
       <div className="contact-page__links">
         <a
           href="https://github.com/Mattia-FR"
           target="_blank"
           rel="noopener noreferrer"
+          className="contact-page__link"
         >
-          GitHub
+          <img src={github} alt="GitHub" />
+          <p>GitHub</p>
         </a>
         <a
           href="https://linkedin.com/in/mattia-bouvier"
           target="_blank"
           rel="noopener noreferrer"
+          className="contact-page__link"
         >
-          LinkedIn
+          <img src={linkedin} alt="LinkedIn" />
+          <p>LinkedIn</p>
         </a>
       </div>
     </section>
